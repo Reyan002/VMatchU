377493bbb0af63c3f49fb733f1bf8f5345d33c98
hammad <reyana015@gmial.com>
2019-04-27 17:02:27 +0500

    vmatchu

--- app/src/main/java/com/example/vmatchu/myProperty.java
+++ app/src/main/java/com/example/vmatchu/myProperty.java
@@ -1,46 +1,150 @@
 package com.example.vmatchu;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Spinner;
 
-public class myProperty extends AppCompatActivity {
+import java.util.ArrayList;
 
-    Spinner spin;
-    String spin_val;
-    String[] gender = { "All status","For Rent","Want Rent","Want Buy","For Sale" };//array of strings used to populate the spinner
+import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
+import in.galaxyofandroid.spinerdialog.SpinnerDialog;
+
+public class myProperty extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ArrayList<String> status=new ArrayList<>();
+    private ArrayList<String> title=new ArrayList<>();
+
+    private SpinnerDialog spinnerDialog;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_property);//setting layout
-        spin = (Spinner) findViewById(R.id.spinnerid);//fetching view's id
-        //Register a callback to be invoked when an item in this AdapterView has been selected
-        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        setContentView(R.layout.activity_my_property);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        status.add("For Rent");
+        status.add("For Sell");
+        status.add("Want Rent");
+        status.add("Want Buy");
+        status.add("All Status");
 
+        title.add("Home");
+        title.add("Shop");
+        title.add("Hall");
+        title.add("Office");
+        title.add("None");
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> arg0, View arg1,
-                                       int position, long id) {
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
 
-                spin_val = gender[position];//saving the value selected
+                AlertDialog.Builder builder=new AlertDialog.Builder(myProperty.this);
+                View view1=getLayoutInflater().inflate(R.layout.filter,null);
+                Spinner spinner=(Spinner) view1.findViewById(R.id.spinnerid);
+                final EditText tittle_ed=(EditText) view1.findViewById(R.id.proTitle);
 
+                builder.setView(view1);
+                AlertDialog dialog=builder.create();
+                dialog.show();
+
+                spinnerDialog=new SpinnerDialog(myProperty.this,title,"select Item");
+                spinnerDialog.bindOnSpinerListener(new OnSpinerItemClick() {
+                    @Override
+                    public void onClick(String item, int position) {
+                        tittle_ed.setText(item);
+                    }
+                });
+                tittle_ed.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        spinnerDialog.showSpinerDialog();
+                    }
+                });
 
             }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-
-            }
-
         });
-        //setting array adaptors to spinners
-        //ArrayAdapter is a BaseAdapter that is backed by an array of arbitrary objects
-        ArrayAdapter<String> spin_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, gender);
 
-        // setting adapteers to spinners
-        spin.setAdapter(spin_adapter);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_property, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
